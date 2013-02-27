d45bfd483e8e8354ffadb83f8ec74568ca42a16d
Walter Landry <wlandry@caltech.edu>
2013-02-26 17:25:05 -0800

    Add an << operator to Coord_Sys

--- src/Geometry/Coord_Sys.hxx
+++ src/Geometry/Coord_Sys.hxx
@@ -13,6 +13,21 @@ namespace ADQL
   };
 }
 
+inline std::ostream & operator<<(std::ostream &os, const ADQL::Coord_Sys &c)
+{
+  switch(c.frame)
+    {
+    case ADQL::Coord_Sys::Reference_Frame::J2000:
+      os << "J2000";
+      break;
+    case ADQL::Coord_Sys::Reference_Frame::ICRS:
+      os << "ICRS";
+      break;
+    }
+  return os;
+}
+
+
 BOOST_FUSION_ADAPT_STRUCT(ADQL::Coord_Sys,
                           (ADQL::Coord_Sys::Reference_Frame, frame)
                           (ADQL::Coord_Sys::Reference_Position, position))
