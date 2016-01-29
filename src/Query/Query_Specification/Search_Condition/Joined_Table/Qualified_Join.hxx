26caa34ec6055c012415717225d18d4aa61924b5
Walter Landry <wlandry@caltech.edu>
2016-01-29 14:09:38 -0800

    Remove commented out lines.

--- src/Query/Query_Specification/Search_Condition/Joined_Table/Qualified_Join.hxx
+++ src/Query/Query_Specification/Search_Condition/Joined_Table/Qualified_Join.hxx
@@ -9,23 +9,19 @@ class Qualified_Join
 {
 public:
   Table_Correlation table_correlation;
-  // Table_Reference table_reference;
   Join_Suffix join_suffix;
   bool empty () const
   {
     return table_correlation.empty ();
-    // return table_reference.empty ();
   }
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Qualified_Join &j)
 {
   return os << j.table_correlation << " " << j.join_suffix;
-  // return os << j.table_reference << " " << j.join_suffix;
 }
 }
 
 BOOST_FUSION_ADAPT_STRUCT (ADQL::Qualified_Join,
                            (ADQL::Table_Correlation, table_correlation)
-                           // (ADQL::Table_Reference, table_reference)
                            (ADQL::Join_Suffix, join_suffix))
