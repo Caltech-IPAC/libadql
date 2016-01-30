7eec3e7c65b974d98b5d1c3c6137b70567023627
Walter Landry <wlandry@caltech.edu>
2016-01-30 00:31:21 -0800

    Add a Shape visitor to Geometry.  May not be a good idea.

--- src/Query/Query_Specification/Geometry.hxx
+++ src/Query/Query_Specification/Geometry.hxx
@@ -3,6 +3,22 @@
 #include "Geometry/Contains.hxx"
 #include "Geometry/Intersects.hxx"
 
+namespace
+{
+class Geometry_Predicate_Shape_Visitor : public boost::static_visitor<const ADQL::Shape &>
+{
+public:
+  const ADQL::Shape &operator()(const ADQL::Intersects &s) const
+  {
+    return s.shape;
+  }
+  const ADQL::Shape &operator()(const ADQL::Contains &s) const
+  {
+    return s.shape;
+  }
+};
+}
+
 namespace ADQL
 {
 class Geometry
@@ -11,6 +27,11 @@ public:
   typedef boost::variant<Intersects, Contains> Predicate;
   Predicate predicate;
   bool empty () const { return empty_variant (predicate); }
+  const Shape &shape() const
+  {
+    Geometry_Predicate_Shape_Visitor visitor;
+    return boost::apply_visitor (visitor, predicate);
+  }
 };
 
 inline std::ostream &operator<<(std::ostream &os,
