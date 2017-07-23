71aca0e7112f88e7eff101ceac2d0e8557793553
Walter Landry <wlandry@caltech.edu>
2017-07-22 17:25:01 -0700

    Decompose Numeric_Value_Expression into term, operator, and expression.

--- src/Query/Query_Specification/Value_Expression/Numeric_Value_Expression.hxx
+++ src/Query/Query_Specification/Value_Expression/Numeric_Value_Expression.hxx
@@ -11,16 +11,20 @@ namespace ADQL
 class Numeric_Value_Expression
 {
 public:
-  std::string expression;
+  std::string term, plus_or_minus, expression;
 };
 
 inline std::ostream &
 operator<<(std::ostream &os,
            const ADQL::Numeric_Value_Expression &numeric_value_expression)
 {
-  return os << numeric_value_expression.expression;
+  return os << numeric_value_expression.term
+            << numeric_value_expression.plus_or_minus
+            << numeric_value_expression.expression;
 }
 }
 
 BOOST_FUSION_ADAPT_STRUCT (ADQL::Numeric_Value_Expression,
-                           (std::string, expression))
+                           (std::string, term)(std::string,
+                                               plus_or_minus)(std::string,
+                                                              expression))
