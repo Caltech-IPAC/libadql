b8d0afff414eb5747dd81e7429a36f451f2a0f8b
Walter Landry <wlandry@caltech.edu>
2017-07-23 18:48:39 -0700

    Add Value_Expression_Wrap

---
+++ src/Query/Query_Specification/Value_Expression/Value_Expression_Wrap.hxx
@@ -0,0 +1,10 @@
+#include <boost/variant/recursive_wrapper.hpp>
+
+#include <iostream>
+
+namespace ADQL
+{
+class Value_Expression;
+typedef boost::recursive_wrapper<Value_Expression> Value_Expression_Wrap;
+std::ostream &operator<<(std::ostream &os, const Value_Expression_Wrap &s);
+}
