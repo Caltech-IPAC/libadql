c3e222f3f2f1be9ab2c0e7361b95e72d46f93ba0
Walter Landry <wlandry@caltech.edu>
2017-08-22 18:09:54 -0700

    Add missing Value_Expression_Non_Bool_Term.hxx

---
+++ src/Query/Query_Specification/Value_Expression_Non_Bool_Term.hxx
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Factor.hxx"
+
+namespace ADQL
+{
+class Value_Expression_Non_Bool_Term
+{
+public:
+  std::string operator_string;
+  Factor factor;
+  bool empty () const { return operator_string.empty (); }
+};
+
+inline std::ostream &operator<<(
+    std::ostream &os,
+    const ADQL::Value_Expression_Non_Bool_Term &value_expression_non_bool_term)
+{
+  return os << value_expression_non_bool_term.operator_string
+            << value_expression_non_bool_term.factor;
+}
+}
+
+BOOST_FUSION_ADAPT_STRUCT (ADQL::Value_Expression_Non_Bool_Term,
+                           (std::string, operator_string)(ADQL::Factor,
+                                                          factor))
