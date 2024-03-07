42e0c8300c6bac3d20feba7f008baf2291f1cb42
Judith Silverman <judith@ipac.caltech.edu>
2024-03-07 12:53:03 -0800

    IRSA-5856 follow-up: Revert Shape and add Shape_or_Column typedef

--- src/Query/Query_Specification/Geometry/Contains.hxx
+++ src/Query/Query_Specification/Geometry/Contains.hxx
@@ -10,16 +10,18 @@ public:
     typedef boost::variant<Point, Column_Reference> Point_or_Column;
 
     Point_or_Column point_or_column;
-    Shape shape;
+    Shape_or_Column shape_or_column;
 
     bool empty() const { return empty_variant(point_or_column); }
 };
 
 inline std::ostream &operator<<(std::ostream &os, const ADQL::Contains &contains) {
-    os << "CONTAINS(" << contains.point_or_column << "," << contains.shape << ")";
+    os << "CONTAINS(" << contains.point_or_column << "," << contains.shape_or_column
+       << ")";
     return os;
 }
 }  // namespace ADQL
 
-BOOST_FUSION_ADAPT_STRUCT(ADQL::Contains, (ADQL::Contains::Point_or_Column,
-                                           point_or_column)(ADQL::Shape, shape))
+BOOST_FUSION_ADAPT_STRUCT(ADQL::Contains,
+                          (ADQL::Contains::Point_or_Column,
+                           point_or_column)(ADQL::Shape_or_Column, shape_or_column))
--- src/Query/Query_Specification/Geometry/Intersects.hxx
+++ src/Query/Query_Specification/Geometry/Intersects.hxx
@@ -8,16 +8,18 @@ namespace ADQL {
 class Intersects {
 public:
     Value_Expression_Wrap expression;
-    Shape shape;
+    Shape_or_Column shape_or_column;
 
     bool empty() const { return ADQL::empty(expression); }
 };
 
 inline std::ostream &operator<<(std::ostream &os, const ADQL::Intersects &intersects) {
-    os << "INTERSECTS(" << intersects.expression << "," << intersects.shape << ")";
+    os << "INTERSECTS(" << intersects.expression << "," << intersects.shape_or_column
+       << ")";
     return os;
 }
 }  // namespace ADQL
 
 BOOST_FUSION_ADAPT_STRUCT(ADQL::Intersects,
-                          (ADQL::Value_Expression_Wrap, expression)(ADQL::Shape, shape))
+                          (ADQL::Value_Expression_Wrap,
+                           expression)(ADQL::Shape_or_Column, shape_or_column))
